992549376c2e93f6f0760ed670ba17bc87740ff6
akanshakul <135011689+akanshakul@users.noreply.github.com>
2023-06-01 18:12:22 +0530

    Add files via upload
    initial commit

---
+++ src/Component/css.js
@@ -0,0 +1,12 @@
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+.header{
+    height: 70px;
+    width: 100%;
+    background-color: whitesmoke;
+    box-shadow: 1px 1px 1px gray;
+    float: left;
+}
---
+++ src/Home.js
@@ -0,0 +1,178 @@
+import React from 'react'
+import './App.css';
+const Home = () => {
+  return (
+    <>
+    <div className="header">
+	<div className='he1'>
+	<img src="https://www.companieslogo.com/img/orig/ABNB_BIG-9ccc2025.png?t=1633511992" className='he2'/>
+    
+	</div>
+    <div className="a1">
+			<pre>       <span className="a2" ><b> Stays</b></span>	    
+                <span className="a3" >  Experiences</span>     
+                  <span className="a4 " >Online Experiences</span></pre>
+		</div>
+        <div className="b1">
+			<img src="https://tse2.mm.bing.net/th?id=OIP.HfXtvnZ8YoT0b3elURJaKgHaHx&pid=Api&P=0&h=180" className="b2"/>
+		</div>
+        </div>
+        <hr/>
+        <div class="header2">
+    <div className="c1 ">
+      <img src="https://cdn-icons-png.flaticon.com/128/747/747327.png"  className="c2 "/>
+      <img src="https://cdn-icons-png.flaticon.com/128/864/864685.png"  className="c3"/>
+    </div>
+     <div classNames="d1" >
+      
+        <select className="d2">  
+          <option>Anywhere</option>
+          <option>Ujjain</option>
+          <option>Indore</option>
+        <option>Bhopal</option>
+        <option>Mumbai</option>
+        </select>
+         <p className="d3"  href="#">
+          <p> june 14-21</p>  
+                  
+                </p>
+        <div className="d4">
+          <button className="d5">+</button>
+          <button className="d6"> 4 guest</button>
+          <button className="d7">-</button>
+          
+        </div>
+  </div>
+      <div className="d8" >
+             <img src="https://tse4.mm.bing.net/th?id=OIP.gbyMSR0sReaRak1xixc6xQHaHa&pid=Api&P=0&h=180"  className="d9" />
+
+      </div>
+      </div>  
+
+      <div className="main">
+      
+      <div className="e1" >
+        <nav className="e2">
+          <ul>
+  
+          <li><img src="https://cdn-icons-png.flaticon.com/128/2111/2111307.png"  className="e3"/>
+          <p className="f1 ">All Home</p></li>
+          <li><img src="https://cdn-icons-png.flaticon.com/128/10637/10637677.png"  className="e4" />
+            <p className="f2 ">Tiny Html </p></li>
+          <li><img src="https://cdn-icons-png.flaticon.com/128/1830/1830767.png" className="e5"/> 
+            <p className="f3 ">Chef's Kitchen</p></li>
+          <li><img src="https://cdn-icons-png.flaticon.com/128/3090/3090246.png" className="e6"/>
+            <p className="f4 ">Surfing</p></li>
+          <li><img src="https://cdn-icons-png.flaticon.com/128/3562/3562699.png" className='e7'/>
+            <p className="f5 ">Mansions</p></li>
+          <li><img src="https://cdn-icons-png.flaticon.com/128/1087/1087934.png" className='e8'/>
+            <p className="f6 ">Luxe</p></li>
+          <li><img src="https://cdn-icons-png.flaticon.com/128/3978/3978412.png" className='e9'/>
+            <p className="f7 ">Treehouse</p></li>
+          <li><img src="https://cdn-icons-png.flaticon.com/128/1054/1054194.png" className='e10'/>
+            <p className=" f8">Camping</p></li>
+          <li><img src="https://cdn-icons-png.flaticon.com/128/1086/1086426.png" className='e11'/>
+            <p className="f9 ">Beachfront</p></li>
+          <li><img src="https://cdn-icons-png.flaticon.com/128/3319/3319235.png" className='e12'/>
+            <p className="f10 ">Farms</p></li>
+           <li><img src="https://cdn-icons-png.flaticon.com/128/2919/2919302.png" className='e13'/>
+            <p className="f11">Castles</p></li> 
+          <li><img src="https://cdn-icons-png.flaticon.com/128/4698/4698218.png" className='e14'/>
+            <p className="f12">Vieyards</p></li>
+          <li><img src="https://cdn-icons-png.flaticon.com/128/3437/3437732.png" className='e15' />
+            <p className="f13 ">Amazing Pools</p></li>
+          <li><img src="https://cdn-icons-png.flaticon.com/128/99/99607.png" className='e16'/>
+            <p className=" f14 ">Sking</p></li>
+            <li><img src="https://cdn-icons-png.flaticon.com/128/780/780363.png" className='e17'/> 
+             <p className="f15 ">Desert</p></li>  
+          </ul>
+         </nav>
+         </div>    
+    
+<div>
+ 			<div className="item"  id='r1'>
+					<img src="https://tse4.mm.bing.net/th?id=OIP.k6DS2e-4aIgUlkVzlc0_KgHaE8&pid=Api&P=0&h=180" className="r2"/>
+					 <h2> Whitefish Estate</h2><br></br>
+						<p>  Whitefish, United State</p><p><br></br>
+         <br></br> <b>$10,000</b>/night
+						</p>
+				</div>
+
+
+        	<div className="item" id="r15" >
+					<img src="https://tse1.mm.bing.net/th?id=OIP.rrBupMvjk4q2YxnIrBYZPQHaE8&pid=Api&P=0&h=180" className="r2"/>
+					<h2> Luxury stay in Weston,Saint James ,Barbados	</h2><p>Weston, Saint James</p>		<p><b>$1,500</b>/night</p>
+				</div>
+
+        <div   className="item" id="r15">
+					<img src="https://tse1.mm.bing.net/th?id=OIP.VVnfHdCBubtikx6o3lTW1gHaC5&pid=Api&P=0&h=180" className='r2'/>
+					<h2>7,500 sq ft Private Beachfront Estate</h2><br></br><p> Dubai,United Arab Emiratess</p><br></br><p><b>$2,478</b>/night</p>
+				</div>
+
+        <div   className="item" id="r15">
+					<img src="https://tse4.mm.bing.net/th?id=OIP.QJAxTmJh3NMrC63W1dAa0wHaC9&pid=Api&P=0&h=180" className='r2'/>
+          <h2>Numero 22-Lake Como-Design Living & Lake View</h2><p> Laglio,Lombardia.italy</p><br></br><p><b>$359</b>/night</p>
+				
+				</div>
+  
+
+        	<div    className="item" id="r1">
+					<img src="https://tse4.mm.bing.net/th?id=OIP.S3QWO7MN5HSgBxN9L0sJnwAAAA&pid=Api&P=0&h=180" className='r2'/>
+      <h2> Designer A-Frama Cabin in the Trees</h2><br></br><p>Lake Arrowhead California,United </p><br></br><br></br><p><b>$225</b>/night</p>
+				</div>
+        
+
+        	<div    className="item" id="r15">
+					<img src="https://tse4.mm.bing.net/th?id=OIP.GRnFm9digD2QnQaT_XABaQHaEK&pid=Api&P=0&h=180" className='r2'/>
+					<h2>Fully Renovated 2 BR on River w/Pool-Walk to To</h2><br></br><p> Aspen,Colorado,United State</p><br></br><p><b>$546</b>/night</p>
+				</div>
+
+      
+
+        	<div    className="item" id="r15">
+					<img src="https://tse1.mm.bing.net/th?id=OIP.ElXMBEF8FfIXC-tQOupibwHaE7&pid=Api&P=0&h=180" className='r2'/>
+					<h2> Bohemian Spacious /Private Pool and Garden</h2><br></br><p>Tultum, Quintana Roo,Mexio</p><br></br><p><b>$199</b>/night</p>
+				</div>
+    
+
+
+        <div   className="item " id="r15">
+					<img src="https://tse4.mm.bing.net/th?id=OIP.ILPI2eHTY6aN79ySq6X4wgHaD4&pid=Api&P=0&h=180" className='r2'/>
+					<h2> Magnolia's Hillcrest Cottage</h2><br></br><p>Waco,Texas,United States</p><br></br><br></br><p><b>$500</b>/night</p>
+				</div>
+
+      
+        <div  className="item "id="r1">
+					<img src="https://tse4.mm.bing.net/th?id=OIP.zl45wCiZg3URCJ7o7cklfwHaEK&pid=Api&P=0&h=180" className='r2'/>
+					<h2>ATELIER ArtVillas Costa Rica</h2><br></br><p>Uvita,Osa,Puntaneras,costa Rica</p><br></br><p><b>$830</b>/night</p>
+				</div>
+
+      
+				<div  className="item "id="r15">
+					<img src="https://tse4.mm.bing.net/th?id=OIP.2EgDuwKarpojyhHOm7te_AHaFj&pid=Api&P=0&h=180" className='r2'/>
+					<h2>Mountainside Luxary Cabin with Batulao </h2><br></br><p>Calaca ,calabarzor, philippines</p><br></br><p><b>$483</b>/night</p>
+				</div>
+
+     
+        	<div  className="item "id="r15">
+					<img src="https://tse4.mm.bing.net/th?id=OIP.J5QRlifsejx-kVvQTIjNoQHaFi&pid=Api&P=0&h=180" className='r2'/>
+					<h2>Kecamatan Ubad,indonesia</h2><br></br><p>Stay with Andy Painter</p><br></br><p><b>$730</b>/night</p>
+				</div>
+
+      
+
+        <div  className="item "id="r15">
+					<img src="https://tse1.mm.bing.net/th?id=OIP.SEFqRptr9XS_ZQmX5CSn_AHaFj&pid=Api&P=0&h=180" className='r2'/>
+					<h2>Levallois-Perret,France</h2><br></br><p>Stay with Annie</p><br></br><br></br><p><b>$830</b>/night</p>
+				</div>
+
+</div>
+
+
+            </div>
+            </>
+
+  )
+}
+
+export default Home
